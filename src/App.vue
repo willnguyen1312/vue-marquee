412853460c8ed60d8a362258f3eb4f5b85fe8006
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-12-31 11:53:50 +0700

    chore: add direction support

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { useRafFn, useToggle } from "@vueuse/core";
 import { nanoid } from "nanoid";
 
 const translateX = ref(0);
+const direction = ref<"left" | "right">("right");
+
 const list = ref<HTMLDivElement>();
 const [isHover, toggleIsHover] = useToggle(false);
 
@@ -27,6 +29,15 @@ useRafFn(() => {
   }
 });
 
+const style = computed(() => {
+  const bound = list.value?.clientWidth || 0;
+  const newValue =
+    direction.value === "left" ? -translateX.value : translateX.value - bound;
+  return {
+    transform: `translateX(${newValue}px)`,
+  };
+});
+
 const images: { src: string; id: string }[] = Array.from(
   { length: 8 },
   (_, index) => ({ src: `/${index + 1}.jpeg`, id: nanoid() })
@@ -44,16 +55,12 @@ const handlePointerLeave = () => {
 <template>
   <div class="m-auto flex flex-col items-center px-4">
     <h1 class="my-10 text-4xl font-bold">Demo Marquee</h1>
-
     <div
       @pointerenter="handlePointerEnter"
       @pointerleave="handlePointerLeave"
       class="overflow-hidden w-full md:w-8/12 cursor-pointer"
     >
-      <div
-        class="flex will-change-transform"
-        :style="{ transform: `translateX(-${translateX}px)` }"
-      >
+      <div class="flex will-change-transform" :style="style">
         <div ref="list" class="flex flex-shrink-0 py-4 relative">
           <img
             v-for="image in images"
