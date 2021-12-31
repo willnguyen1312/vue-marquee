9bc602e356f742a53315ddf453cf25708a714437
Nam Nguyen <nam.nguyen.node@gmail.com>
2021-12-31 12:35:03 +0700

    🧹

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { ref, computed } from "vue";
-import { useRafFn, useToggle } from "@vueuse/core";
+import { useRafFn } from "@vueuse/core";
 import { nanoid } from "nanoid";
 
 const translateX = ref(0);
 const direction = ref<"left" | "right">("left");
 
 const list = ref<HTMLDivElement>();
-const [isHover, toggleIsHover] = useToggle(false);
+const isHover = ref(false);
 
 const pixelsMovedPerFrameOnIdle = 1.5;
 const pixelsMovedPerFrameOnHover = 0.5;
@@ -42,22 +42,14 @@ const images: { src: string; id: string }[] = Array.from(
   { length: 8 },
   (_, index) => ({ src: `/${index + 1}.jpeg`, id: nanoid() })
 );
-
-const handlePointerEnter = () => {
-  toggleIsHover();
-};
-
-const handlePointerLeave = () => {
-  toggleIsHover();
-};
 </script>
 
 <template>
   <div class="m-auto flex flex-col items-center px-4">
     <h1 class="my-10 text-4xl font-bold">Demo Marquee</h1>
     <div
-      @pointerenter="handlePointerEnter"
-      @pointerleave="handlePointerLeave"
+      @pointerenter="isHover = true"
+      @pointerleave="isHover = false"
       class="overflow-hidden w-full md:w-8/12 cursor-pointer"
     >
       <div class="flex will-change-transform" :style="style">
